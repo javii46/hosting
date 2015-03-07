c4f5b19c24a55a065f56167dcd424b0c4f962dc8
root <root@debian>
2015-03-07 18:49:42 +0100

    creacion virtual host

--- alta.py
+++ alta.py
@@ -22,5 +22,19 @@ else:
 #creamos el document root del usuario junto al index.html    
         os.system("mkdir /var/www/%s" %nombre)
         os.system("cp /home/vagrant/index.html /var/www/%s"%nombre)
-
+#crearemos el nuevo virtualhost
+                virtual_host="/home/vagrant/virtual_host"
+                modolectura=open(virtual_host, "r")
+                modoescritura = open(virtual_host+'.mod', "w")
+                buff = modolectura.read()
+                char1='%nombre%'
+                rbuff = buff.replace(char1, nombre)
+                modoescritura.write(rbuff)
+                modolectura.close()
+                modoescritura.close()
+#movemos plantilla a sites-available
+                os.system("mv /home/vagrant/virtual_host.mod /etc/apache2/sites-available/www.%s"%ndominio)
+#activamos el modulo y reiniciamos apache
+                activar=os.system("a2ensite www.%s"%ndominio)
+                reiniciar=os.system("service apache2 restart")
 
